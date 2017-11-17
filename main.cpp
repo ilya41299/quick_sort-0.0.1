80c9e113769baad98dde926f186220d7edacedb9
ilya41299 <32190925+ilya41299@users.noreply.github.com>
2017-11-17 13:09:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,70 @@
 #include <iostream>
 #include <sstream>
+#include <utility>
 using namespace std;
+
+bool read (istream & stream, int *mas, int &size) {
+    bool res = true;
+    for (int i=0; i < size; i++) {
+        if (!(stream >> mas[i])) {
+            res = false;
+            break;
+        }
+    }
+    return res;
+}
 
 void q_sort (int *mas, int left, int right ) {
     int c_left = left;
     int c_right = right;
     int p = c_left;
     while (c_left <= c_right) {
-        while (mas[c_right] > mas [p] && c_right >= left ){
-          right--;
-        }
-        while ( (mas[c_left] < mas [p]) && (c_left <= right) ){
-            left++;
-        }
+        while (mas[c_right] > mas[p] && c_right > left )
+            c_right--;
+        
+        while ( (mas[c_left] < mas[p]) && (c_left < right) )
+            c_left++;
+        
         if (c_left <=c_right){
             swap(mas[c_left], mas [c_right]);
             c_left++;
             c_right--;
         }
     }
-        if (c_left < right) q_sort(mas, c_left, right);
-        if (c_right > left) q_sort(mas, left, c_right);
+    if (left < c_right) {
+        q_sort(mas, left, c_right);
+    }
+    if (c_left < right) {
+        q_sort(mas, c_left, right);
+    }
 }
 
 int main() {
     int size;
-    int *mas = new int [size];
-    if (cin >> size){
-        if (size < 0){
-        cout << "An error has occured while reading input data.";
-        return -1;
+
+
+    if(!(cin >> size)) {
+        cout << "An error has occured while reading numbers from line";
+        return 1;
+    }
+    int *mas;
+
+    cin.get();
+    string string;
+    getline(cin, string);
+    istringstream stream(string);
+    mas = new int [size];
+
+    if(read(stream, mas, size)){
+
+        q_sort (mas, 0, size-1);
+        for(int i=0; i<size; i++){
+            cout << mas[i] << " ";
         }
-        mas = new int [size];
+        delete[]mas;
     }
     else {
-        cout<<"error"<<endl;
-        return -1;
-    }
-    cin.get();
-    string stroka;
-    getline (cin, stroka);
-    istringstream stream (stroka);
-    for (int i=0; i<size;i++) {
-        if (!(stream >> mas[i])) {
-            cout << "An error has occured while reading input data." << endl;
-            delete[]mas;
-            return -1;
-        }
-    }
-    int left = mas[0];
-    int right = mas [size-1];
-    q_sort (mas, left, right);
-    for(int i=0; i<size; i++){
-        cout << mas[i] << " ";
+        cout << "An error has occured while reading numbers from line";
     }
     delete[]mas;
     return 0;
